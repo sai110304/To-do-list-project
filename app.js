80ca0157d20655ad66c6973f99f315c92f1d374b
sai110304 <cs21b042@iittp.ac.in>
2023-06-16 13:36:51 +0530

    changed the port

--- app.js
+++ app.js
@@ -156,6 +156,6 @@ app.post('/delete',async function (req,res) {
 // })
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server started on port 3000.");
 });
